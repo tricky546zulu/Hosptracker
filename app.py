6fac33ac47cefdc431b2a8c0d0e07f8ec6460c3a
tricky546zulu <Sean546mc@proton.me>
2025-06-27 20:15:56 -0600

    Update app.py.

--- app.py
+++ app.py
@@ -13,11 +13,13 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("SESSION_SECRET", "dev-key")
 
 # Database configuration
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
-    "pool_recycle": 300,
-    "pool_pre_ping": True,
-}
+#app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+#app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+  #  "pool_recycle": 300,
+   # "pool_pre_ping": True,
+#}
+import os
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///hospitals.db')
 
 # Initialize database
 db.init_app(app)
