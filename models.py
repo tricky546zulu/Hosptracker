86073d9308cb27e2b94288109655e9b022825556
tricky546zulu <Sean546mc@proton.me>
2025-07-03 19:42:08 -0600

    Update models.py

--- models.py
+++ models.py
@@ -1,16 +1,22 @@
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from extensions import db
 
-db = SQLAlchemy()
+class Hospital(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(10), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    data = db.relationship('HospitalData', backref='hospital', lazy=True)
 
 class HospitalData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    hospital_code = db.Column(db.String(10), nullable=False)
-    patient_count = db.Column(db.Integer, nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    hospital_id = db.Column(db.Integer, db.ForeignKey('hospital.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    inpatient_beds = db.Column(db.Integer)
+    overcapacity_beds = db.Column(db.Integer)
+    total_patients = db.Column(db.Integer)
+    waiting_for_inpatient_bed = db.Column(db.Integer)
 
 class ScrapingLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, nullable=False)
     status = db.Column(db.String(50), nullable=False)
     message = db.Column(db.Text, nullable=True)
